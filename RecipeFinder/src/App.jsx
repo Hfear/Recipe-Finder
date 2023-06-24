c82cfccf39a5f23e43bafdec9161568a6ff3696a
Hfear <hannahbfearns@gmail.com>
2023-06-24 15:30:11 -0400

    basic grid and component files

--- RecipeFinder/src/App.jsx
+++ RecipeFinder/src/App.jsx
@@ -2,16 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import RecipeView from './RecipeView'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <RecipeView/>
+
+
+
    
     </>
   )
---
+++ RecipeFinder/src/recipes.js
@@ -0,0 +1,41 @@
+//an array of recipe objects,,
+//hold the actual inner info of the recipies
+
+const recipes = [
+
+    {
+        image : {
+            src :"https://images.unsplash.com/photo-1578985545062-69928b1d9587?ixid=M3wxMjA3fDB8MXxzZWFyY2h8NXx8Y2FrZXxlbnwwfHx8fDE2ODc1NTcwNzd8MA&ixlib=rb-4.0.3&dpr=1&auto=format&fit=crop&w=120&h=200&q=60",
+            alt: "chocolat cake",
+        },
+        name : "chocolate cake",
+        category: "cake",
+
+    },
+
+    {
+        image : {
+            src :"https://images.unsplash.com/photo-1606313564200-e75d5e30476c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGRlc3NlcnRzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+            alt: "brownies",
+        },
+        name : "chocolate brownies",
+        category: "brownie",
+
+    },
+
+    {
+        image : {
+            src :"https://images.unsplash.com/photo-1630782622540-26a9f8c7a095?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
+            alt: "vanilla shortbread",
+        },
+        name : "vanilla shortbread",
+        category: "cookies",
+
+    },
+
+];
+
+export default recipes;
+
+
+
