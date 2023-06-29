9db75408b925308d6ec1b6de5197c843f2d749ed
Hfear <hannahbfearns@gmail.com>
2023-06-28 21:35:51 -0400

    upload recipe

--- RecipeFinder/src/App.jsx
+++ RecipeFinder/src/App.jsx
@@ -109,10 +109,10 @@ function App() {
 
     const response = await fetch("http://127.0.0.1:3000/recipes", {
       method: "POST",
-      Headers: {
+      headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({a : 1}),
+      body: JSON.stringify(recipeForm),
 
     });
     
