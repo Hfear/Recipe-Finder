5e829657d5a8fb9072b9cb032a62a9d8dab22503
Hfear <hannahbfearns@gmail.com>
2023-07-05 14:09:30 -0400

    reviews shown

--- RecipeFinder/src/routes/Recipe.jsx
+++ RecipeFinder/src/routes/Recipe.jsx
@@ -1,5 +1,5 @@
-import { Link, useLoaderData } from "react-router-dom";
-import { FaPenAlt, FaTrash} from "react-icons/fa";
+import { Link, useLoaderData, Form} from "react-router-dom";
+import { FaPenAlt, FaRegTrashAlt} from "react-icons/fa";
 
 // import{categorybyId} from "../utils"
 
@@ -7,12 +7,17 @@ import { FaPenAlt, FaTrash} from "react-icons/fa";
 export async function loader({params}){
   const response = await fetch(`http://localhost:3000/recipes/${params.recipeId}`);
   const recipe = await response.json();
-  return {recipe};
+
+  const reviewsResponse = await fetch(`http://localhost:3000/reviews?recipeId=${params.recipeId}`);
+  const reviews = await reviewsResponse.json();
+  console.log(reviews)
+
+  return {recipe, reviews};
 }
 
 export default function Recipe(){
 
-  const { recipe } = useLoaderData();
+  const { recipe, reviews } = useLoaderData();
 
   const{
     id,
@@ -23,9 +28,38 @@ export default function Recipe(){
     description,
 } = recipe; 
 
+const mappedReviews = reviews.map((review) => {
+  return(
+    <>
+    <div
+    className="flex gap-4 border border-solid border-pink-900 m-3">
+      <div
+      className="" > 
+      {review.username} - </div>
+      <div> 
+      "{review.review}" </div>
+
+      <Form
+       method="post"
+       action={`/reviews${recipe.id}/destroy?recipebId=${id}`}
+       onSubmit={(event) => {
+         if (!confirm("Please confirm you want to delete this review.")) {
+           event.preventDefault();
+         }
+       }}
+      >
+      <button> <FaRegTrashAlt/></button>
+      </Form>
+      
+      </div>
+     
+    </>
+  )
+})
+
 return (
-  <>
-        <Link to={"/home"}> {'<'} Back </Link>
+<>
+<Link to={"/home"}> {'<'} Back </Link>
 <h1>{name}</h1>
 
 <Link to = {`/recipes/${id}/edit`}
@@ -36,9 +70,9 @@ className="gap-2 p-20 m-1">
 {image ? <img className = "r-img" src={image.src} alt={image.alt} /> : <img className = "r-img" src="https://placehold.co/100x100" alt="No company logo available" />}
 <h5 className = "r-category">{category}</h5>
 <h4>{ingredients}</h4>
-<h2>{description}</h2>
+<h2> {description}</h2>
 
-
+<div> {mappedReviews}</div>
   </>
   )
 }
