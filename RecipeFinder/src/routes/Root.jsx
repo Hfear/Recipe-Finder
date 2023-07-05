1db19ddf78e00a1b3fda2b633482f8cd54dd1ac2
Hfear <hannahbfearns@gmail.com>
2023-07-05 16:02:28 -0400

    some styles

--- RecipeFinder/src/routes/Root.jsx
+++ RecipeFinder/src/routes/Root.jsx
@@ -1,10 +1,12 @@
 import { NavLink, Outlet, Link } from "react-router-dom";
+import Navbar from "../navbar";
 
 export default function Root() {
     return (
       <>
+      <Navbar/>
 
-      <nav class="bg-white border-gray-200 dark:bg-gray-900">
+      {/* <nav class="bg-white border-gray-200 dark:bg-gray-900">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="https://flowbite.com/" class="flex items-center">
               <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
@@ -39,7 +41,7 @@ export default function Root() {
             </ul>
           </div>
         </div>
-      </nav>
+      </nav> */}
 
 
 
